c8ceda590cd5eae2ac66f4ed9524b7bd8ba3bdc7
itsDavidev <davidlez2mgk@gmail.com>
2022-05-16 12:41:28 -0400

    📝 page projects

---
+++ src/constants/pageProyects.data.js
@@ -0,0 +1,70 @@
+// dblog
+import dblo1 from '../assets/images/dblog1.png'
+import dblo2 from '../assets/images/dblog2.png'
+import dblo3 from '../assets/images/dblog3.png'
+
+// davtion
+import davtion1 from '../assets/images/davtion1.png'
+import davtion2 from '../assets/images/davtion2.png'
+import davtion3 from '../assets/images/davtion3.png'
+
+// Art Gallery
+import art1 from '../assets/images/art1.png'
+import art2 from '../assets/images/art2.png'
+import art3 from '../assets/images/art3.png'
+
+export const pageProyectsData = [
+    {
+        name: "Ddblog",
+        images: [
+            dblo1,
+            dblo2,
+            dblo3
+        ],
+        description: '',
+        code: "https://github.com/itsDavidev/Dblogv1",
+        preview: "https://dblogv1strapi.netlify.app",
+        technologies: [
+            "11ty",
+            "Nunjucks",
+            "PostCSS",
+            "JavaScript",
+            "Css"
+        ]
+    },
+    {
+        name: "Davtion",
+        images: [
+            davtion1,
+            davtion2,
+            davtion3
+        ],
+        description: "",
+        code: "https://github.com/itsDavidev/DavTionDev",
+        preview: "https://davtion.netlify.app",
+        technologies: [
+            "React",
+            "Graphql",
+            "Apollo Client",
+            "TypeScript",
+            "PostCss",
+        ]
+    },
+    {
+        name: "Art Gallery",
+        images: [
+            art1,
+            art2,
+            art3
+        ],
+        description: "",
+        code: "https://github.com/itsDavidev/artGallery",
+        preview: "https://bright-buttercream-6df396.netlify.app/",
+        technologies: [
+            "React",
+            "TypeScript",
+            "Sass",
+            "Axios"
+        ]
+    },
+]
