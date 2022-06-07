26ccb45e7727840cea33f850affaceb4e769eac8
itsdavidev <davidevlez2mgk@gmail.com>
2022-06-07 12:26:38 -0500

    📱 refactor desing

---
+++ src/_data/habilidades.data.js
@@ -0,0 +1,67 @@
+import js from '../assets/icons/technologies/js.svg'
+import react from '../assets/icons/technologies/react.svg'
+import ts from '../assets/icons/technologies/ts.svg'
+import css from '../assets/icons/technologies/css.svg'
+import postcss from '../assets/icons/technologies/postcss.svg'
+import nodejs from '../assets/icons/technologies/nodejs.svg'
+import webcomponets from '../assets/icons/technologies/webcomponents.svg'
+import git from '../assets/icons/technologies/git.svg'
+import mongoDB from '../assets/icons/technologies/mongodb.svg'
+import graphql from '../assets/icons/technologies/graphql.svg'
+import tailwindcss from '../assets/icons/technologies/tailwind.svg'
+import sass from '../assets/icons/technologies/sass.svg'
+import vue from '../assets/icons/technologies/vue.svg'
+
+
+
+export const habilidadesFrontEnd = [
+    {
+        name: 'JavaScript',
+        image: js,
+    }, {
+        name: "TypeScript",
+        image: ts,
+    }, {
+        name: "React",
+        image: react,
+    }, {
+        name: "Vue",
+        image: vue,
+    }, {
+        name: "Css",
+        image: css,
+    }, {
+        name: "PostCss",
+        image: postcss,
+    }, {
+        name: "Sass",
+        image: sass,
+    }, {
+        name: "Tailwind Css",
+        image: tailwindcss,
+    }
+]
+
+export const habilidadesBackEnd = [
+    {
+        name: 'Node.Js',
+        image: nodejs,
+    }, {
+        name: 'MongoDB',
+        image: mongoDB,
+    }, {
+        name: 'GraphQL',
+        image: graphql,
+    }
+]
+
+
+export const habilidaadesExtra = [
+    {
+        name: 'Git',
+        image: git,
+    }, {
+        name: 'Web Components',
+        image: webcomponets,
+    },
+]
